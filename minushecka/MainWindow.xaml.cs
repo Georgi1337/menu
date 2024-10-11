97a207fda4c52e77194e19cc2424afc5b41d5a40
Georgi1337 <melnikovgeorgii6@gmail.com>
2024-10-11 17:03:13 +0500

    Уже немного

--- minushecka/MainWindow.xaml.cs
+++ minushecka/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace minushecka
         {
             InitializeComponent();
         }
+
+        private void meIt1_Click(object sender, RoutedEventArgs e)
+        {
+            meIt1.Background = Brushes.DarkBlue;
+        }
+
+        private void meIt2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Легенда гласит о творце всего сущего(ну тоесть о саздателе этой WpF) когда-то давно он ... ладно хватит и этого");
+        }
+
+        private void meIt3_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tol1_Click(object sender, RoutedEventArgs e)
+        {
+            tol1.Background = Brushes.DarkBlue;
+        }
+
+        private void tol2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Легенда гласит о творце всего сущего(ну тоесть о саздателе этой WpF) когда-то давно он ... ладно хватит и этого");
+        }
+
+        private void tol3_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
